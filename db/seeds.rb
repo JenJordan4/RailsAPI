159f7fa349c230756edfc8f8c4cc8d6c4f3659b1
jenjordan4 <jbaldwin4@hotmail.com>
2020-03-10 11:16:39 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,239 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+app1 = MyApp.create(
+  name: "my-app-001"
+)
+
+app2 = MyApp.create(
+  name: "my-app-002"
+)
+
+app3 = MyApp.create(
+  name: "my-app-003"
+)
+
+app4 = MyApp.create(
+  name: "my-app-004"
+)
+
+app5 = MyApp.create(
+  name: "my-app-005"
+)
+
+app6 = MyApp.create(
+  name: "my-app-006"
+)
+
+app7 = MyApp.create(
+  name: "my-app-007"
+)
+
+app8 = MyApp.create(
+  name: "my-app-008"
+)
+
+app9 = MyApp.create(
+  name: "my-app-009"
+)
+
+app10 = MyApp.create(
+  name: "my-app-010"
+)
+
+app11 = MyApp.create(
+  name: "my-app-011"
+)
+
+app12 = MyApp.create(
+  name: "my-app-012"
+)
+
+app13 = MyApp.create(
+  name: "my-app-013"
+)
+
+app14 = MyApp.create(
+  name: "my-app-014"
+)
+
+app15 = MyApp.create(
+  name: "my-app-015"
+)
+
+app16 = MyApp.create(
+  name: "my-app-016"
+)
+
+app17 = MyApp.create(
+  name: "my-app-017"
+)
+
+app18 = MyApp.create(
+  name: "my-app-018"
+)
+
+app19 = MyApp.create(
+  name: "my-app-019"
+)
+
+app20 = MyApp.create(
+  name: "my-app-020"
+)
+
+app21 = MyApp.create(
+  name: "my-app-021"
+)
+
+app22 = MyApp.create(
+  name: "my-app-022"
+)
+
+app23 = MyApp.create(
+  name: "my-app-023"
+)
+
+app24 = MyApp.create(
+  name: "my-app-024"
+)
+
+app25 = MyApp.create(
+  name: "my-app-025"
+)
+
+app26 = MyApp.create(
+  name: "my-app-026"
+)
+
+app27 = MyApp.create(
+  name: "my-app-027"
+)
+
+app28 = MyApp.create(
+  name: "my-app-028"
+)
+
+app29 = MyApp.create(
+  name: "my-app-029"
+)
+
+app30 = MyApp.create(
+  name: "my-app-030"
+)
+
+app31 = MyApp.create(
+  name: "my-app-031"
+)
+
+app32 = MyApp.create(
+  name: "my-app-032"
+)
+
+app33 = MyApp.create(
+  name: "my-app-033"
+)
+
+app34 = MyApp.create(
+  name: "my-app-034"
+)
+
+app35 = MyApp.create(
+  name: "my-app-035"
+)
+
+app36 = MyApp.create(
+  name: "my-app-036"
+)
+
+app37 = MyApp.create(
+  name: "my-app-037"
+)
+
+app38 = MyApp.create(
+  name: "my-app-038"
+)
+
+app39 = MyApp.create(
+  name: "my-app-039"
+)
+
+app40 = MyApp.create(
+  name: "my-app-040"
+)
+
+app41 = MyApp.create(
+  name: "my-app-041"
+)
+
+app42 = MyApp.create(
+  name: "my-app-042"
+)
+
+app43 = MyApp.create(
+  name: "my-app-043"
+)
+
+app44 = MyApp.create(
+  name: "my-app-044"
+)
+
+app45 = MyApp.create(
+  name: "my-app-045"
+)
+
+app46 = MyApp.create(
+  name: "my-app-046"
+)
+
+app47 = MyApp.create(
+  name: "my-app-047"
+)
+
+app48 = MyApp.create(
+  name: "my-app-048"
+)
+
+app49 = MyApp.create(
+  name: "my-app-049"
+)
+
+app50 = MyApp.create(
+  name: "my-app-050"
+)
+
+app51 = MyApp.create(
+  name: "my-app-051"
+)
+
+app52 = MyApp.create(
+  name: "my-app-052"
+)
+
+app53 = MyApp.create(
+  name: "my-app-053"
+)
+
+app54 = MyApp.create(
+  name: "my-app-054"
+)
+
+app55 = MyApp.create(
+  name: "my-app-055"
+)
+
+app56 = MyApp.create(
+  name: "my-app-056"
+)
+
+app57 = MyApp.create(
+  name: "my-app-057"
+)
+
+app58 = MyApp.create(
+  name: "my-app-058"
+)
+
+app59 = MyApp.create(
+  name: "my-app-059"
+)
+
+app60 = MyApp.create(
+  name: "my-app-060"
+)
